02bf20b72c6ccedf331adbd72fa67d5cd3aa147c
paliandris02 <pali.andris@gmail.com>
2023-05-10 00:10:25 +0200

    model, view, controller files

---
+++ Models/RewardPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XpressKuponXpressKupon.Models
+{
+	public class RewardPoint
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; }
+		public int Points { get; set; }
+		public int PointsHeld { get; set; }
+		public DateTime TransactionTime { get; set; }
+		public int StoreId { get; set; }
+	}
+}
