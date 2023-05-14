ae526c0f19380103c5487898391c2a42530aebfa
paliandris02 <pali.andris@gmail.com>
2023-05-14 15:55:12 +0200

    view is ok, but no data

---
+++ Services/JutalomPontService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using XpressKuponXpressKupon.DAL;
+using XpressKuponXpressKupon.Models;
+
+
+namespace XpressKuponXpressKupon.Services
+{
+	public class RewardsPointService
+	{
+		public IEnumerable<JutalomPont> GetRewardsPoints()
+		{
+			var repository = new RewardsPointRepository();
+			return repository.GetRewardsPoints();
+		}
+	}
+}
